64f281aa551e529256441d1712c44187e1277c54
De Meyer Romanie <serena@mb-de-serena.home>
2022-06-06 13:08:32 +0200

    img

--- next.config.js
+++ next.config.js
@@ -3,9 +3,5 @@ module.exports = {
     WEATHER_API_KEY: process.env.WEATHER_API_KEY,
     HOSTNAME: process.env.HOSTNAME,
   },
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
   trailingSlash: true
 };
